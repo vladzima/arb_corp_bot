0fd702edc6843149ea57a6f7022d68348c711e3b
Vlad Arbatov <mail@vladislav.tv>
2016-11-04 17:50:06 +0300

    Clients 2

--- behavior/scripts/index.js
+++ behavior/scripts/index.js
@@ -33,14 +33,26 @@ exports.handle = (client) => {
     }
   })
 
+  const handleClients = client.createStep({
+  satisfied() {
+    return false
+  },
+
+  prompt() {
+    client.addTextResponse('We are proud to work with some very nice clients.')
+    client.done()
+  }
+})
+
   client.runFlow({
     classifications: {
-      // map inbound message classifications to names of streams
+      clients: 'clients'
     },
     autoResponses: {
       // configure responses to be automatically sent as predicted by the machine learning model
     },
     streams: {
+      clients: handleClients,
       main: 'onboarding',
       onboarding: [sayHello],
       end: [untrained],
